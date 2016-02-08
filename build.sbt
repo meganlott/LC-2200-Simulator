412c05d324aa952f5599418a88ec68b77a9301a6
Nathan Braswell <miloignis@gmail.com>
2016-02-08 13:52:26 -0500

    Swap to ScalaFX and SBT

---
+++ build.sbt
@@ -0,0 +1,17 @@
+
+
+val scalafx = "org.scalafx" %% "scalafx" % "8.0.60-R9"
+
+lazy val commonSettings = Seq(
+  organization := "xyz.room409.lc2200",
+  version := "1.33.7"
+)
+
+lazy val root = (project in file(".")).
+settings(commonSettings: _*).
+settings(
+  name := "lc2200-simulator",
+  libraryDependencies += scalafx
+)
+
+
--- gui.scala
+++ gui.scala
@@ -1,22 +1,49 @@
-import scala.swing._
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
 
-class UI extends MainFrame {
-  title = "GUI Program #1";
-  preferredSize = new Dimension(640, 480);
-  contents = new Label("Here is the contents!");
-  contents = new FlowPanel { 
-    contents += new Button("huhu") { 
-      minimumSize = preferredSize
-      maximumSize = preferredSize
-      preferredSize = preferredSize
+object HelloStageDemo extends JFXApp {
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+    width = 600
+    height = 450
+    scene = new Scene {
+      fill = Color.LightGreen
+      content = new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when (hover) choose Color.Green otherwise Color.Red
+      }
     }
   }
 }
 
-object GuiProgramOne {
-  def main(arg: Array[String]) {
-    val ui = new UI
-    ui.visible = true
-    println("End of main function.")
-  }
-}
+
+
+
+//import scala.swing._
+
+//class UI extends MainFrame {
+  //title = "GUI Program #1";
+  //preferredSize = new Dimension(640, 480);
+  //contents = new Label("Here is the contents!");
+  //contents = new FlowPanel { 
+    //contents += new Button("huhu") { 
+      //minimumSize = preferredSize
+      //maximumSize = preferredSize
+      //preferredSize = preferredSize
+    //}
+  //}
+//}
+
+//object GuiProgramOne {
+  //def main(arg: Array[String]) {
+    //val ui = new UI
+    //ui.visible = true
+    //println("End of main function.")
+  //}
+//}
